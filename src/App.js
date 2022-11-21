f381e1fee9fc358645757c6236fd632550d68885
Ragrats- <91531302+ragrats1403@users.noreply.github.com>
2022-11-22 02:05:55 +0800

    Test

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import LoginPage from './Components/LoginPage';
+import UserDashboard from './Components/UserDashboard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { render } from '@testing-library/react';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Routes>   
+        <Route path='/' element={<LoginPage />} />
+        </Routes>
+    </Router>
+    
+    </>
+  );
+  }
+
+
+export default App;
